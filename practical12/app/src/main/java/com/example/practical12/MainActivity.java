6810b3d0e47d8283d14a58134dacb58aa0a3c249
samruddhipatil-25 <samruddhipatil254@gmail.com>
2026-05-05 09:26:05 +0530

    Add files via upload

---
+++ practical12/app/src/main/java/com/example/practical12/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.practical12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            imageView.setImageResource(R.drawable.image1);
+        });
+    }
+}
