d8e415acec3f5559d8db64f4947169f2c94238b3
samruddhipatil-25 <samruddhipatil254@gmail.com>
2026-05-05 09:22:49 +0530

    Add files via upload

---
+++ practical9/app/src/main/java/com/example/practical9/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.practical9;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.practical9.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
